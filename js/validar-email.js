dfd7ff1b7cf8b5e8ad3193ef8739c67b4dbc854c
Edivandro Lima <ml596923@gmail.com>
2020-05-22 22:40:25 -0300

    Primeiro commit

---
+++ js/validar-email.js
@@ -0,0 +1,38 @@
+function validarEmail(field) {
+	// Email
+	var e= field.value;
+	// document
+	var id_invalido= document.getElementById('email-invalid');
+	var id_email= document.getElementById('email');
+	var id_enviar= document.getElementById('enviar');
+	// If e Else
+	if (e != "") {
+		if ((e.indexOf('@') == -1) ||
+			(e.indexOf('.') == -1) ||
+			(e.indexOf(' ') != -1) ||
+			(e.split('@')[1].indexOf('.') == -1)) {
+			id_invalido.innerHTML="Please provide a valid email";
+			id_email.style.borderColor="hsl(0, 93%, 68%)";
+			id_email.style.backgroundPosition="right 95px center";
+		}
+		else {
+			id_invalido.innerHTML="";
+			id_email.style.borderColor="#d6d6d6";
+			id_enviar.type="submit";
+			id_email.style.backgroundPosition="right 0px center";
+		}
+	}
+	else {
+		id_invalido.innerHTML="";
+		id_email.style.borderColor="#d6d6d6";
+		id_email.style.backgroundPosition="right 0px center";
+	}
+}
+// Check form
+function check_form(e) {
+	if (e.value == "") {
+		document.getElementById('email-invalid').innerHTML="Please provide a valid email";
+		document.getElementById('email').style.borderColor="hsl(0, 93%, 68%)";
+		document.getElementById('email').style.backgroundPosition="right 95px center";
+	}
+}
